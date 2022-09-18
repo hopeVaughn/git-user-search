fc2f84ccb3dc986c3082fb85d99524baa5b37c46
hopeVaughn <hopevaughnwarren@gmail.com>
2022-09-18 11:33:45 -0700

    add card for user

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,8 +2,37 @@ import React from 'react';
 import { GithubContext } from '../context/context';
 import styled from 'styled-components';
 import { MdBusiness, MdLocationOn, MdLink } from 'react-icons/md';
+
 const Card = () => {
-  return <h2>card component</h2>;
+  const { githubUser } = React.useContext(GithubContext);
+  const {
+    avatar_url,
+    html_url,
+    name,
+    company,
+    blog,
+    bio,
+    location,
+    twitter_username
+  } = githubUser;
+  return (
+    <Wrapper>
+      <header>
+        <img src={avatar_url} alt={name} />
+        <div>
+          <h4>{name}</h4>
+          <p>{twitter_username ? `@${twitter_username}` : 'no twitter provided'}</p>
+        </div>
+        <a href={html_url}>follow</a>
+      </header>
+      <p className='bio'>{bio ? `${bio}` : 'no bio provided'}</p>
+      <div className='links'>
+        <p><MdBusiness />{company ? `${company}` : 'no company provided'}</p>
+        <p><MdLocationOn />{location ? `${location}` : 'no location provided'}</p>
+        <a href={`https://${blog}`}><MdLink />{blog ? `${blog}` : 'no blog provided'}</a>
+      </div>
+    </Wrapper>
+  )
 };
 const Wrapper = styled.article`
   background: var(--clr-white);
