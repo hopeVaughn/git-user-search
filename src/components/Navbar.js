ccf2b1a232def19664529f4103812a7cf4668d85
hopeVaughn <hopevaughnwarren@gmail.com>
2022-09-20 13:09:59 -0700

    add authentication api

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,20 @@ import styled from 'styled-components';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const Navbar = () => {
+  const { isAuthenticated, loginWithRedirect, logout, user, isLoading } = useAuth0()
+  const isUser = isAuthenticated && user;
+
   return (
-    <Wrapper>navbar</Wrapper>
+    <Wrapper>
+      {isUser && user.picture && <img src={user.picture} alt={user.name} />}
+      {isUser && user.name && <h4>welcome, <strong>{user.name.toUpperCase()}</strong></h4>}
+      {isUser ? (
+        <button onClick={() => { logout({ returnTo: window.location.origin }) }}>logout</button>
+      ) : (
+        <button onClick={loginWithRedirect}>login</button>
+      )
+      }
+    </Wrapper>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -5,12 +5,18 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { GithubProvider } from './context/context';
 import { Auth0Provider } from '@auth0/auth0-react';
-
+// 
 ReactDOM.render(
   <React.StrictMode>
-    <GithubProvider>
-      <App />
-    </GithubProvider>
+    <Auth0Provider
+      domain='dev-xifn4c1x.us.auth0.com'
+      clientId="WLH5pR0dpXdRRmZgM3sTTv1ynlWRnSty"
+      redirectUri={window.location.origin}
+    >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
