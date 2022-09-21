1555af7be67fb0bb78f343bb1f8d6c1e12392ae9
hopeVaughn <hopevaughnwarren@gmail.com>
2022-09-21 11:29:52 -0700

    clean up comments and small fixes to readability

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { GithubContext } from '../context/context';
-import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
+import { Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
 
 
 const Repos = () => {
@@ -52,20 +52,7 @@ const Repos = () => {
 
   stars = Object.values(stars).slice(-5).reverse();
   forks = Object.values(forks).slice(-5).reverse();
-  // const chartData = [
-  //   {
-  //     label: "HTML",
-  //     value: "13"
-  //   },
-  //   {
-  //     label: "CSS",
-  //     value: "160"
-  //   },
-  //   {
-  //     label: "JavaScript",
-  //     value: "80"
-  //   },
-  // ];
+
   return <>
 
     <section className='section'>
